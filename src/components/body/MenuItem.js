b6577051bd3eef164d2c4acc04778645d84cd032
limonrana <limonrana515@gmail.com>
2021-04-18 01:43:35 +0600

    Designing Menu Item

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,6 +1,19 @@
-const MenuItem = (props) => {
-    console.log(props);
-    return <div>working</div>;
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+
+const MenuItem = ({ dish }) => {
+    const { name, image } = dish;
+    return (
+        <div>
+            <Card style={{ margin: '10px' }}>
+                <CardBody>
+                    <CardImg width="100%" alt={name} src={image} style={{ opacity: '0.5' }} />
+                    <CardImgOverlay>
+                        <CardTitle>{name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
+        </div>
+    );
 };
 
 export default MenuItem;
